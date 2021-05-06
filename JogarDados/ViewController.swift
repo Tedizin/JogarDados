a6643cb059f8dc7020e0d7efd43bde89ffdbfcdd
Felipe Lima <felipe_limacarvalho@hotmail.com>
2021-05-06 17:20:35 -0400

    Created - Main Interface

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dadoImageView1: UIImageView!
 
+    @IBOutlet weak var dadoImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dadoImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        dadoImageView1.alpha = 0.5
     }
-
-
 }
 
