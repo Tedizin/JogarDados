d932577eb562d27e9ed1960a53af2a35a14a64c7
edyama <ey.yamagata@gmail.com>
2021-05-06 17:12:13 -0400

    Inserimos imagens e conectamos ao código

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    @IBOutlet weak var dadoImageView1: UIImageView!
+    @IBOutlet weak var dadoImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        dadoImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        
+        dadoImageView1.alpha = 0.5
     }
 
 }
+
 
