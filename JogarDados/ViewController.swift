33a9cd3958eb381cc449c0b1bcb31ed8ebcf6c63
Paolasoares <paolaalcantara1@yahoo.com.br>
2021-05-06 20:07:01 -0400

    atualizando imagens

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var dado1: UIImageView!
+    
+    @IBOutlet weak var dado2: UIImageView!
+    
+    @IBAction func botao(_ sender: Any) {
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        dado1.image = #imageLiteral(resourceName: "DiceFive")
     }
 
 }
