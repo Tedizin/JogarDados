ce3fc3e98e8a1331f8284b10f74c4989e222f92e
edyama <ey.yamagata@gmail.com>
2021-05-10 17:33:12 -0400

    UIImage utilizado

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -13,25 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var dadoImageView1: UIImageView!
     @IBOutlet weak var dadoImageView2: UIImageView!
     
-    var selectDado1 = 1
-    var selectDado4 = 4
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        dadoImageView2.image = #imageLiteral(resourceName: "DiceSix")
     }
     
     @IBAction func rolarDadosPressed(_ sender: UIButton) {
-        if selectDado1 < 6 {
-            dadoImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][selectDado1]
-            dadoImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][selectDado4]
-                
-            selectDado1 += 1
-            selectDado4 -= 1
-        }
+        let arrayDados = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive")]
         
-        //dadoImageView1.image = #imageLiteral(resouceName: "DiceFour")
+        dadoImageView1.image = arrayDados[Int.random(in: 0...5)]
+        dadoImageView2.image = arrayDados[Int.random(in: 0...5)]
     }
 }
 
