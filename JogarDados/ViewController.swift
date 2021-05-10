5fda7a4d5794a6e720e53b5a693d5bb950d276c5
Andressa Alves Tenorio <andressa.a.tenorio@gmail.com>
2021-05-10 17:44:19 -0400

    Aprendendo Recursos

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -11,17 +11,19 @@ class ViewController: UIViewController {
     
     //MARK: - @IDOutlet
     
-    @IBOutlet weak var diceOneUIImageView: UIImageView!
-    @IBOutlet weak var diceTwoUIImageView: UIImageView!
-    @IBAction func rollDiceUIButton(_ sender: Any) {
-    }
+    @IBOutlet weak var dadoImageView1: UIImageView!
+    @IBOutlet weak var dadoImageView2: UIImageView!
     
-    //MARK: - Lifecycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
 
+    //MARK - Actions
+    @IBAction func rolarDadosPressed(_ sender: UIButton) {
+        //print("Botao Rolar dados Pressionado")
+        let arrayDados = [UIImage(named:"DiceOne"),UIImage(named:"DiceTwo"),UIImage(named:"DiceThree"),UIImage(named:"DiceFour"),UIImage(named:"DiceFive"),UIImage(named:"DiceSix")]
+
+        dadoImageView1.image = arrayDados[Int.random(in: 0...5)]
+        dadoImageView2.image = arrayDados[Int.random(in: 0...5)]
+      
+    }
+    
 }
-
+// Exercícios: Fazer com que quando precione o botao rolar dados os dois dados estejam no numero 4.
