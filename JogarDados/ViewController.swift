496148cce73acb6e897255bb3058f6bd4b6c5c4f
edyama <ey.yamagata@gmail.com>
2021-05-10 16:04:50 -0400

    Editar app e corrigir bugs

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -13,14 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var dadoImageView1: UIImageView!
     @IBOutlet weak var dadoImageView2: UIImageView!
     
+    var selectDado1 = 1
+    var selectDado4 = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dadoImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        
-        dadoImageView1.alpha = 0.5
+        dadoImageView2.image = #imageLiteral(resourceName: "DiceSix")
     }
-
+    
+    @IBAction func rolarDadosPressed(_ sender: UIButton) {
+        if selectDado1 < 6 {
+            dadoImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][selectDado1]
+            dadoImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][selectDado4]
+                
+            selectDado1 += 1
+            selectDado4 -= 1
+        }
+        
+        //dadoImageView1.image = #imageLiteral(resouceName: "DiceFour")
+    }
 }
 
-
+// Fazer com que quando o app é aberto os dois dados estejam no número quatro.
