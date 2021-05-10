dbc0e643602e4f0b590a7d6585b2fb9c6b8c205e
Henrique Silva de Carvalho <henrique.silvac@outlook.com>
2021-05-10 18:55:41 -0300

    Reconfig random dice

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
         
         let arrayDice = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
         
-        diceOneUIImageView.image = arrayDice.randomElement() as? UIImage
+        diceOneUIImageView.image = arrayDice[Int.random(in: 0...5)]
         diceTwoUIImageView.image = arrayDice.randomElement() as? UIImage
         
     }
